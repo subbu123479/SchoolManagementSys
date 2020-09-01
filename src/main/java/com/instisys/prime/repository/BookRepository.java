07eb1980839c1bcff3ee9c41003727b5e2453bfd
subbu123479 <subbu123479@gmail.com>
2020-09-01 17:22:49 +0530

    School Management System

--- src/main/java/com/instisys/prime/repository/BookRepository.java
+++ src/main/java/com/instisys/prime/repository/BookRepository.java
@@ -1,8 +1,10 @@
 package com.instisys.prime.repository;
 
+import com.instisys.prime.entity.Faculty;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -10,7 +12,9 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.instisys.prime.entity.Book;
 
-public interface BookRepository extends PagingAndSortingRepository<Book, Long>{
+import java.util.List;
+@RepositoryRestResource(collectionResourceRel="book", path="book")
+public interface BookRepository extends JpaRepository<Book, Long>{
 	
 
 
@@ -21,4 +25,11 @@ public interface BookRepository extends PagingAndSortingRepository<Book, Long>{
 
 	@RestResource(path = "searchbykeyword")
 	Page<Book> findByNameContaining(@Param("name") String keyword, Pageable pageable);
+
+	@Query("FROM Book b WHERE b.active = true")
+	List<Book> findByActive(@Param("active") String active);
+
+	@Query("FROM Book b WHERE b.unitsInStock > 0 and b.active = true")
+	List<Book> findByUnitsInStockAndActive();
+
 }
